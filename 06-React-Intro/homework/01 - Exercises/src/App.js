6fcbd78c5a2eff81f23dcc33e5ef6d322d1fd380
Samayoa <fsamayoal@gmail.com>
2023-08-18 19:10:06 -0600

    tarea 6

--- 06-React-Intro/homework/01 - Exercises/src/App.js
+++ 06-React-Intro/homework/01 - Exercises/src/App.js
@@ -4,7 +4,9 @@ import Bienvenido from './components/Bienvenido.jsx';
 export default function App() {
    return (
       <div>
+                          
          <Bienvenido />
+         
       </div>
    );
 }
--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-const studentName = "";
+const studentName = "Fernando Samayoa Landin";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
+<div>
+  <h1>Estudiante</h1>
+  <h3>{techSkills}</h3>
+</div>
+
   return;
 }
 
