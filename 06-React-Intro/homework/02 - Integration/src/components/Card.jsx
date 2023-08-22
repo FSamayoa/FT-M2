a188dd72ef2b23610155ab127728cc21bdabf84d
Samayoa <fsamayoal@gmail.com>
2023-08-21 18:07:00 -0600

    Tarea 6

--- 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,13 +1,13 @@
-export default function Card(props) {
+export default function Card({name, species, gender, image, onClose}) {
    return (
       <div>
-         {/* <button onClick={}>X</button>
+         <button onClick={onClose}>X</button>
+         <h2>name: {name}</h2>
+         <h2>Species: {species}</h2>
+         <h2>Gender: {gender}</h2>
          <h2></h2>
          <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <img src={} alt='' /> */}
+         <img src={image} alt='' />
       </div>
    );
 }
